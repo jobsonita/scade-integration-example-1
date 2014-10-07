9ce9b480068387c0368bc67807dfe871ac207571
Jobson Gilberto Barros Amorim <jobsonita@gmail.com>
2014-10-07 07:54:01 -0300

    Fix - Both teams implemented the basic structure for communication through the framework

--- Team1/IntegrationCode/team1UA.c
+++ Team1/IntegrationCode/team1UA.c
@@ -13,7 +13,6 @@ operator_input_type ua_inputs;
 operator_output_type ua_outputs;
 
 void receiveMessage(FRAMEWORK_MESSAGE message) {
-    /*
     TEAM1_INPUT_INTERFACE input;
 
     if (message.to == TEAM1) {
@@ -21,43 +20,34 @@ void receiveMessage(FRAMEWORK_MESSAGE message) {
         switch (message.from) {
         case TEAM2:
             printf("Received: Message from TEAM2 to TEAM1 \n");
-            //ua_inputs.SignalFromTeam2 = input.SignalFromTeam2;
+            /*ua_inputs.SignalFromTeam2 = input.SignalFromTeam2;*/
             break;
         }
     }
-    */
 }
 
 void buildMessage(FRAMEWORK_MESSAGE *message) {
-    /*
     message->from = TEAM1;
     switch (message->to) {
     case TEAM2:
         printf("Sent: Message from TEAM1 to TEAM2 \n");
         TEAM2_INPUT_INTERFACE *output = &(message->input_interface.team2_input_interface);
-        //output->SignalFromTeam1 = ua_outputs.SignalToTeam2;
+        /*output->SignalFromTeam1 = ua_outputs.SignalToTeam2;*/
     }
-    */
 }
 
 void executeOperator() {
-    /*
     project1(&ua_inputs, &ua_outputs);
-    */
 }
 
 void clear_ua_inputs() {
-    /*
     ua_receive_clear(&ua_inputs, NULL);
 
     ua_inputs.SignalFromTeam2 = FALSE;
-    */
 }
 
 void clear_ua_outputs() {
-    /*
     project1_reset(&ua_outputs);
-    */
 }
 
 void initializeCustomLogic() {
--- Team2/IntegrationCode/team2UA.c
+++ Team2/IntegrationCode/team2UA.c
@@ -13,7 +13,6 @@ operator_input_type ua_inputs;
 operator_output_type ua_outputs;
 
 void receiveMessage(FRAMEWORK_MESSAGE message) {
-    /*
     TEAM2_INPUT_INTERFACE input;
 
     if (message.to == TEAM2) {
@@ -21,43 +20,34 @@ void receiveMessage(FRAMEWORK_MESSAGE message) {
         switch (message.from) {
         case TEAM1:
             printf("Received: Message from TEAM1 to TEAM2 \n");
-            //ua_inputs.SignalFromTeam1 = input.SignalFromTeam1;
+            /*ua_inputs.SignalFromTeam1 = input.SignalFromTeam1;*/
             break;
         }
     }
-    */
 }
 
 void buildMessage(FRAMEWORK_MESSAGE *message) {
-    /*
     message->from = TEAM2;
     switch (message->to) {
     case TEAM1:
         printf("Sent: Message from TEAM2 to TEAM1 \n");
         TEAM1_INPUT_INTERFACE *output = &(message->input_interface.team1_input_interface);
-        //output->SignalFromTeam2 = ua_outputs.SignalToTeam1;
+        /*output->SignalFromTeam2 = ua_outputs.SignalToTeam1;*/
     }
-    */
 }
 
 void executeOperator() {
-    /*
     project2(&ua_inputs, &ua_outputs);
-    */
 }
 
 void clear_ua_inputs() {
-    /*
     ua_receive_clear(&ua_inputs, NULL);
 
     ua_inputs.SignalFromTeam1 = FALSE;
-    */
 }
 
 void clear_ua_outputs() {
-    /*
     project2_reset(&ua_outputs);
-    */
 }
 
 void initializeCustomLogic() {
